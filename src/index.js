6a751b81b22322d2e1936d33b0c454e08b5d9221
armind04 <purpleskyemusic@gmail.com>
2021-03-10 23:59:53 +0200

    feat: change index file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (matrix = []) {
+  let res = [];
+
+  matrix.forEach((item, index) => {
+    if(index % 2) item = item.reverse();
+    item.forEach(i => res.push(i));
+  })
+
+  return res;
 }
